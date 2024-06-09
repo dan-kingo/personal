8e327327fb434481884d315f83401fe289c91af1
Dan Kingo <danieldejen23@gmail.com>
2024-06-09 11:03:09 +0300

    fix conact section

--- js/index.js
+++ js/index.js
@@ -143,3 +143,49 @@
    * Initiate Pure Counter
    */
 })();
+
+// adding form functionality
+
+const form = document.getElementById("form");
+const result = document.getElementById("result");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+  result.innerHTML = "Please wait...";
+
+  fetch("https://api.web3forms.com/submit", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: json,
+  })
+    .then(async (response) => {
+      let json = await response.json();
+      if (response.status == 200) {
+        result.innerHTML = "Form submitted successfully";
+        result.style.color = "teal";
+      } else {
+        console.log(response);
+        result.innerHTML = json.message;
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      result.innerHTML = "Something went wrong!";
+      result.style.color = "crimson";
+    })
+    .then(function () {
+      form.reset();
+      setTimeout(() => {
+        result.style.display = "none";
+      }, 3000);
+    });
+});
+
+let year = new Date().getFullYear();
+document.getElementById("year").innerText = year;
